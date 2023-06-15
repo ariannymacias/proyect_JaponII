d63efcf689ef17e00bbc428e866413cfd85eb44a
JaponII <JaponII@gmail.com>
2023-06-15 00:38:15 -0400

    first commit

---
+++ manual/SQL/sistema_011442.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 03-06-2023 a las 19:13:28
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `sistema`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id` int(11) NOT NULL,
+  `usuario` varchar(40) NOT NULL,
+  `password` varchar(33) NOT NULL,
+  `nombre` varchar(80) NOT NULL,
+  `tipo_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id`, `usuario`, `password`, `nombre`, `tipo_usuario`) VALUES
+(1, 'admin', 'admin', 'Administrador web', 1),
+(2, 'usuario', 'usuario', 'usuario', 2);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ manual/sistema/login.php
@@ -0,0 +1,56 @@
+<?php
+
+require_once ("d://xampp/htdocs/sistema/head.php");
+
+?>
+
+
+
+
+
+
+<div class="fondo-login">
+
+<div class="icon">
+    <a href="/login/index.php">
+    <i class="fa-solid fa-user  dog-icon"  ></i>
+    </a>
+</div>
+    <div class="titulo">
+        Inicia Sesion
+    </div>
+    <form action="verificar.php" method ="POST" class=" col-3 login" autocomplete="off">
+        <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">Correo</label>
+            <input type="email" name="correo" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            
+        </div>
+        <div class="mb-3">
+            <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+            <div class="box-eye">
+                <button  type="button" onclick="mostrarContraseña('password', 'eyepassword')">
+                    <i  id="eyepassword" class="fa-solid fa-eye changePassword"></i>
+                </button>
+            </div>
+            <input type="password" name="contraseña" class="form-control" id="password">
+        </div>
+        
+        <div class="d-grid gap-2" >
+            <button type="submit" class="btn btn-primary">Login</button>
+        </div>
+    </form>
+    <div class="col-3 login mt-3">
+        Crear Cuenta <a href="singout.php" style= "text-decoration: none;">click aqui</a>
+    </div>
+</div>
+
+
+<?php
+
+require_once ("d://xampp/htdocs/sistema/footer.php");
+
+?>
+
+
+
+
